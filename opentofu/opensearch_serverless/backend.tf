c14aeec8938d7bda459ab434b0942517e88ecfef
David Hundley <david.k.hundley@gmail.com>
2024-08-03 17:12:06 -0500

    Updating the AOSS backend

--- opentofu/opensearch_serverless/backend.tf
+++ opentofu/opensearch_serverless/backend.tf
@@ -1,5 +1,17 @@
 terraform {
 
+    # Setting the backend remote
+    backend "remote" {
+
+        # Setting the Scalr hostname and organization (environment) name
+        hostname = "dkhundley.scalr.io"
+        organization = "dkhundley-aws"
+
+        workspaces {
+            name = "dkhundley-opensearch-vectorstore"
+        }
+    }
+
     # Downloading the required resource providers
     required_providers {
         aws = {
