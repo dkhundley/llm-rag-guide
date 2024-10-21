0ed642c84ba115b00181ab5adce5694bc85f7e38
David Hundley <36610221+dkhundley@users.noreply.github.com>
2024-10-21 20:58:11 +0000

    Refactor IAM and security policies for OpenSearch Serverless

--- opentofu/opensearch_serverless/iam.tf
+++ opentofu/opensearch_serverless/iam.tf
@@ -1,15 +1,10 @@
-## DATA BLOCKS
-## --------------------------------------------------------------------------------------------------------------
-
-
-
 ## RESOURCE BLOCKS
 ## --------------------------------------------------------------------------------------------------------------
 # Creating an encryption security policy
 resource "aws_opensearchserverless_security_policy" "encryption_policy" {
     name = "${var.resource_prefix}-encryption-policy"
     type = "encryption"
-    description = "Policy for encryption at rest for ${var.resource_prefix} OpenSearch Service domain"
+    description = "Policy for encryption at rest for ${locals.collection_name} OpenSearch Serverless collection"
     policy = jsonencode({
         Rules = [
             {
@@ -23,37 +18,80 @@ resource "aws_opensearchserverless_security_policy" "encryption_policy" {
     })
 }
 
+
+
 # Creating a network security policy
 resource "aws_opensearchserverless_security_policy" "network_security_policy" {
     name = "${var.resource_prefix}-network-security-policy"
     type = "network"
     policy = jsonencode([
         {
-            Description = "VPC access for collection endpoint",
+            Description = "Allowing access from any resource in the AWS account",
             Rules = [
                 {
                     ResourceType = "collection",
                     Resource = [
-                        "collection/${var.collection_name}"
+                        "collection/${locals.collection_name}"
                     ]
                 }
             ],
             AllowFromPublic = false,
-            SourceVPCEs = [
-                aws_opensearchserverless_vpc_endpoint.vpc_endpoint.id
-            ]
+            SourceVPCEs = []
         },
         {
-            Description = "Public access for dashboards",
+            Description = "Allowing access for dashboards from any resource in the AWS account",
             Rules = [
                 {
-                    ResourceType = "dashboard"
+                    ResourceType = "dashboard",
                     Resource = [
-                        "collection/${var.collection_name}"
+                        "collection/${locals.collection_name}"
                     ]
                 }
             ],
-            AllowFromPublic = true
+            AllowFromPublic = false,
+            SourceVPCEs = []
         }
     ])
+}
+
+
+# Creating the data access policy
+resource "aws_opensearchserverless_access_policy" "data_access_policy" {
+    name = "${var.resource_prefix}-data-access-policy"
+    type = "data"
+    policy = jsonencode([
+        {
+            Rules = [
+                {
+                    ResourceType = "index",
+                    Resource = [
+                        "collection/${locals.collection_name}/*"
+                    ],
+                    Permission = [
+                        "aoss:*"
+                    ]
+                },
+                {
+                    ResourceType = "collection",
+                    Resource = [
+                        "collection/${locals.collection_name}"
+                    ],
+                    Permission = [
+                        "aoss:*"
+                    ]
+                }
+            ],
+            Principal = [
+                "*"
+            ]
+        }
+    ])
+}
+
+
+
+# Creating the OpenSearch Serverless collection
+resource "aws_opensearchserverless_collection" "aoss_collection" {
+    name = locals.collection_name
+    depends_on = [ aws_opensearchserverless_security_policy.encryption_policy ]
 }
--- opentofu/opensearch_serverless/locals.tf
+++ opentofu/opensearch_serverless/locals.tf
@@ -1,3 +1,4 @@
 locals {
     prefix = var.resource_prefix
+    collection_name = "${var.resource_prefix}-collection"
 }
