b5070e44c0a2fa7ca871f8ee5d368ad8eac3a4c2
David Hundley <36610221+dkhundley@users.noreply.github.com>
2024-10-21 20:44:14 +0000

    Refactor IAM and security policies for OpenSearch Serverless

--- opentofu/opensearch_serverless/iam.tf
+++ opentofu/opensearch_serverless/iam.tf
@@ -1,69 +1,59 @@
 ## DATA BLOCKS
 ## --------------------------------------------------------------------------------------------------------------
-# Setting the "Assume Role" information
-data "aws_iam_policy_document" "bedrock_kb_assume_role" {
-    statement {
-        actions = ["sts:AssumeRole"]
-        principals {
-            type = "Service"
-            identifiers = ["bedrock.amazonaws.com"]
-        }
-    }
-}
-
-# Setting the IAM policy information
-data "aws_iam_policy_document" "bedrock_kb_policy_document" {
-
-    statement {
-        actions = [
-            "bedrock:CreateKnowledgeBase",
-            "bedrock:GetKnowledgeBase",
-            "bedrock:UpdateKnowledgeBase",
-            "bedrock:DeleteKnowledgeBase",
-            "bedrock:CreateDataSource",
-            "bedrock:GetDataSource",
-            "bedrock:UpdateDataSource",
-            "bedrock:DeleteDataSource",
-            "bedrock:ListKnowledgeBases",
-            "bedrock:ListDataSources",
-            "aoss:CreateCollection",
-            "aoss:DeleteCollection",
-            "aoss:UpdateCollection",
-            "aoss:CreateAccessPolicy",
-            "aoss:CreateSecurityPolicy",
-            "aoss:GetAccessPolicy",
-            "aoss:GetSecurityPolicy",
-            "aoss:ListAccessPolicies",
-            "aoss:ListSecurityPolicies",
-            "aoss:UpdateAccessPolicy",
-            "aoss:UpdateSecurityPolicy",
-            "s3:GetObject",
-            "s3:ListBucket",
-            "ec2:DescribeVpcs",
-            "ec2:DescribeSecurityGroups"
-        ]
-        resources = ["*"]
-    }
-}
 
 
 
 ## RESOURCE BLOCKS
 ## --------------------------------------------------------------------------------------------------------------
-# Creating the IAM role
-resource "aws_iam_role" "bedrock_kb_iam_role" {
-  name = "${local.prefix}-role"
-  assume_role_policy = data.aws_iam_policy_document.bedrock_kb_assume_role.json
+# Creating an encryption security policy
+resource "aws_opensearchserverless_security_policy" "encryption_policy" {
+    name = "${var.resource_prefix}-encryption-policy"
+    type = "encryption"
+    description = "Policy for encryption at rest for ${var.resource_prefix} OpenSearch Service domain"
+    policy = jsonencode({
+        Rules = [
+            {
+                Resource = [
+                    "${aws_opensearchserverless_domain.domain.arn}"
+                ]
+                ResourceType = "collection"
+            }
+        ],
+        AWSOwnedKey = true
+    })
 }
 
-# Creating the IAM policy
-resource "aws_iam_policy" "bedrock_kb_iam_policy" {
-    name = "${local.prefix}-policy"
-    policy = data.aws_iam_policy_document.bedrock_kb_policy_document.json
-}
-
-# Attaching the IAM policy to the IAM role
-resource "aws_iam_role_policy_attachment" "bedrock_kb_policy_attachment" {
-    role = aws_iam_role.bedrock_kb_iam_role.name
-    policy_arn = aws_iam_policy.bedrock_kb_iam_policy.arn
+# Creating a network security policy
+resource "aws_opensearchserverless_security_policy" "network_security_policy" {
+    name = "${var.resource_prefix}-network-security-policy"
+    type = "network"
+    policy = jsonencode([
+        {
+            Description = "VPC access for collection endpoint",
+            Rules = [
+                {
+                    ResourceType = "collection",
+                    Resource = [
+                        "collection/${var.collection_name}"
+                    ]
+                }
+            ],
+            AllowFromPublic = false,
+            SourceVPCEs = [
+                aws_opensearchserverless_vpc_endpoint.vpc_endpoint.id
+            ]
+        },
+        {
+            Description = "Public access for dashboards",
+            Rules = [
+                {
+                    ResourceType = "dashboard"
+                    Resource = [
+                        "collection/${var.collection_name}"
+                    ]
+                }
+            ],
+            AllowFromPublic = true
+        }
+    ])
 }
